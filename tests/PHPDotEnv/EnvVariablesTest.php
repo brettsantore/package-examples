d0e778bdc6de8bbcb8004c31563dc2a26b1743d8
Brett Santore <brett.santore@gmail.com>
2018-06-08 11:56:55 -0400

    refactor example

--- tests/PHPDotEnv/EnvVariablesTest.php
+++ tests/PHPDotEnv/EnvVariablesTest.php
@@ -16,10 +16,15 @@ class EnvVariablesTest extends TestCase
          * This test completely removes the added benefits of this package because it exposes the what we were trying
          * to hide. This test is solely to demonstrate how the package works
          */
-        $example_file   = '.env_example';
+
+
+        /**
+         * This file would never be committed to you VCS
+         */
+        $example_file = '.env';
 
         (new Dotenv(__DIR__, $example_file))->load();
 
-        $this->assertEquals('World', getenv('HELLO'));
+        $this->assertEquals('my_super_secret_password', getenv('DB_PASSWORD'));
     }
 }
